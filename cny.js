9b288359929a36fdde0ba81a14608887a0f6264d
Hong Vin <khvmaths2016@gmail.com>
2023-01-20 09:43:07 +0000

    shortcut

--- cny.js
+++ cny.js
@@ -66,7 +66,7 @@ client.on("message", async (msg) => {
   debug.info('MESSAGE FROM',msg.from,'->',msg.body);
 
   if (msg.body.startsWith("!CNY")) {
-    let content = msg.body.split.substring(5);
+    let content = msg.body.substring(5);
     createImage(content);
     let media = await MessageMedia.fromFilePath("./message1.png");
     await client.sendMessage(msg.from, media);
